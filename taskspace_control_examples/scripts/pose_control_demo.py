639d543a727e35a96b084631913afd66e270a93d
Alex Arbogast <arbogastaw@gmail.com>
2024-11-14 11:20:12 -0500

    change tool orientation for demo robot

--- taskspace_control_examples/scripts/pose_control_demo.py
+++ taskspace_control_examples/scripts/pose_control_demo.py
@@ -12,8 +12,8 @@ from taskspace_control_examples.trajectory import *
 class PoseControlDemo(ControlDemo):
     def __init__(self, setpoint_hz=1000):
         super(PoseControlDemo, self).__init__(setpoint_hz)
-        self.static_orient = Quaternion(0.7071, 0.7071, 0.0, 0.0)
-        self.home = [0.0, -0.845, 1.858, 0.558, 1.571, 0.0]
+        self.static_orient = Quaternion(0.0, 0.0, 0.0, 1.0)
+        self.home = [0.0, -1.125, 2.275, -1.15, 1.571, 0.0]
 
     def run(self):
         self.start_joint_control()
